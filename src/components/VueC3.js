cab0ce6140b1492f08c493c4a727628e0a75d57d
Antti Tupamäki <antupis@gmail.com>
2017-06-12 09:48:11 +0300

    added more props

--- src/components/VueC3.js
+++ src/components/VueC3.js
@@ -31,15 +31,76 @@ export default Vue.extend({
     types: {
       default: () => {},
       type: Object
+    },
+    axes: {
+      default: () => {},
+      type: Object
+    },
+    axis: {
+      default: () => {},
+      type: Object
+    },
+    grid: {
+      default: () => {},
+      type: Object
+    },
+    regions: {
+      default: () => [],
+      type: Array
+    },
+    subchart: {
+      default: () => {},
+      type: Object
+    },
+    zoom: {
+      default: () => {},
+      type: Object
+    },
+    legend: {
+      default: () => {},
+      type: Object
+    },
+    tooltip: {
+      default: () => {},
+      type: Object
+    },
+    color: {
+      default: () => {},
+      type: Object
+    },
+    padding: {
+      default: () => {},
+      type: Object
+    },
+    transition: {
+      default: () => {},
+      type: Object
+    },
+    names: {
+      default: () => {},
+      types: Object
     }
+
   },
   mounted () {
     this.chart = c3.generate({
       bindto: this.$el,
       data: {
         columns: this.columns,
-        types: this.types
-      }
+        types: this.types,
+        axes: this.axes
+      },
+      axis: this.axis,
+      grid: this.grid,
+      regions: this.regions,
+      subchart: this.subchart,
+      zoom: this.zoom,
+      legend: this.legend,
+      tooltip: this.tooltip,
+      color: this.color,
+      padding: this.padding,
+      transition: this.transition,
+      names: this.names
     })
   },
   watch: {
